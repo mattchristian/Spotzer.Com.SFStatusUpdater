53af741e8978a30dc9237733143075faa2f8b486
mattchristian <matty.christian@gmail.com>
2015-02-05 15:30:11 +0100

    SPOTZER-3105

---
+++ SFRestApiManager/Logging/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace SFRestApiManager.Logging
+{
+    internal static class Log
+    {
+        public static Logger Instance { get; set; }
+        static Log()
+        {
+            //Always watch to see if configuration has changed.
+            LogManager.ReconfigExistingLoggers();
+            Instance = LogManager.GetCurrentClassLogger();
+        }
+    }
+}
