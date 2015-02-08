695d2d13b57496b060e9942893f9dbe6f895990d
mattycrocks <matty.christian@gmail.com>
2015-02-08 16:24:46 +0100

    SPOTZER-3105

---
+++ SFRestApiManager.Tests/CommandTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SFRestApiUpdater.Commands;
+
+namespace SFRestApiUpdater.Tests
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void TestArgumentsObjects()
+        {
+            String argument1Name = "-I";
+            String argument1Value = "1";
+            String argument2Name = "-N";
+            String argument2Value = "John Doe";
+            String[] argumentList = new String[2];
+            argumentList[0] = String.Format("{0}={1}", argument1Name, argument1Value);
+            argumentList[1] = String.Format("{0}={1}", argument2Name, argument2Value);
+
+            Arguments actualArguments = new Arguments(argumentList);
+            Assert.AreEqual(argument1Value, actualArguments["I"]);
+            Assert.AreEqual(argument2Value, actualArguments["N"]);
+        }
+
+        [TestMethod]
+        public void TestStatusCallIsNotValid()
+        {
+            String[] argumentList = DefaultArgumentList();
+            argumentList[0] = "-S=";
+            StatusCallCommands commands = new StatusCallCommands(new Arguments(argumentList));
+            Assert.AreEqual(false, commands.IsValid());
+        }
+
+        [TestMethod]
+        public void TestStatusCallIsValid()
+        {
+            var argumentList = DefaultArgumentList();
+            StatusCallCommands commands = new StatusCallCommands(new Arguments(argumentList));
+            Assert.AreEqual(true, commands.IsValid());
+        }
+
+        [TestMethod]
+        public void TestStatusCallConstructor()
+        {
+            var argumentList = DefaultArgumentList();
+            StatusCallCommands commands = new StatusCallCommands(new Arguments(argumentList));
+            Assert.AreEqual("999999", commands.Spon);
+            Assert.AreEqual(99, commands.Status);
+            Assert.AreEqual(false, commands.IsTest);
+            Assert.AreEqual("www.approvalURL.com", commands.ApprovalURL);
+            Assert.AreEqual(String.Empty, commands.FreelancerId);
+            Assert.AreEqual(String.Empty, commands.TaskId);
+            Assert.AreEqual(String.Empty, commands.OrderId);
+            Assert.AreEqual(String.Empty, commands.Role);
+            Assert.AreEqual(true, commands.ProcessOnInsert);
+
+        }
+
+        private String[] DefaultArgumentList()
+        {
+            String[] argumentList = new String[8];
+            argumentList[0] = String.Format("-S={0}", 999999);
+            argumentList[1] = String.Format("-T={0}", 99);
+            argumentList[2] = String.Format("-TE={0}", false);
+            argumentList[3] = String.Format("-U={0}", "www.approvalURL.com");
+            argumentList[4] = String.Format("-FI={0}", "");
+            argumentList[5] = String.Format("-TI={0}", "");
+            argumentList[6] = String.Format("-O={0}", "");
+            argumentList[7] = String.Format("-R={0}", "");
+            return argumentList;
+        }
+    }
+}
---
+++ SFRestApiManager.Tests/ExtensionsTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using SFRestApiUpdater.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Dynamic;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace SFRestApiUpdater.Tests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void TestToJsonString()
+        {
+            String expectedJson = "{\"Id\":1,\"Name\":\"John Doe\"}";
+            Object jsonObject = new { Id = 1, Name = "John Doe" };
+            String actualJson = jsonObject.ToJsonString();
+            Assert.AreEqual(expectedJson, actualJson);
+        }
+
+        [TestMethod]
+        public void TestToDynamicObject()
+        {
+            dynamic expectedObject = new ExpandoObject();
+            expectedObject.Id=1;
+            expectedObject.Name="John Doe";
+            dynamic actualObject = new SimpleTestObject().ToDynamic();
+            Assert.AreEqual(expectedObject.Id, actualObject.Id);
+            Assert.AreEqual(expectedObject.Name, actualObject.Name);
+        }
+
+        [TestMethod]
+        public void TestToDynamicObjectWithAttributes()
+        {
+            dynamic expectedObject = new ExpandoObject();
+            expectedObject.Id = 1;
+            expectedObject.Name = "John Doe";
+
+            dynamic actualObject = new AttributeDecoratedTestObject().ToDynamic();
+            Assert.AreEqual(expectedObject.Id, actualObject.Id__c);
+            Assert.AreEqual(expectedObject.Name, actualObject.Name__c);
+
+            try {
+                var i = actualObject.AlternativeId;
+                Assert.Fail("RuntimeBinderException should have been thrown as Ignore flag on Salesforce Attribute was set.");
+            } catch (Exception ex)
+            { Assert.IsTrue(ex is RuntimeBinderException); }
+
+        }
+    }
+
+    /*
+     * Test Objects
+     */
+
+    class SimpleTestObject
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        
+        public SimpleTestObject()
+        {
+            Id = 1;
+            Name = "John Doe";
+        }
+    }
+
+    class AttributeDecoratedTestObject
+    {
+        [SalesforceAttribute(ApiName="Id__c")]
+        public int Id { get; set; }
+
+        [SalesforceAttribute(ApiName = "Name__c")]
+        public String Name { get; set; }
+
+        [SalesforceAttribute(Ignore=true)]
+        public int AlternativeID { get; set; }
+
+        public AttributeDecoratedTestObject()
+        {
+            Id = 1;
+            Name = "John Doe";
+            AlternativeID = 100;
+        }
+    }
+}
